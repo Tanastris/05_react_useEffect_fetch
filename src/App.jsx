5eacab40a79aa4f442975499f63a67d2778068c3
RokasV <vezikauskas.r@gmail.com>
2024-01-16 08:42:12 +0200

    init + usersList

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import UsersList from './components/users/UsersList';
+import './styles/App.css';
+
+export default function App() {
+  return (
+    <div className='App container'>
+      <h1>Hello, world!</h1>
+      <hr />
+      <UsersList />
+    </div>
+  );
+}
---
+++ src/components/users/UsersList.jsx
@@ -0,0 +1,11 @@
+export default function UsersList() {
+  return (
+    <div>
+      <h2>Users</h2>
+      <ul>
+        <li>user1</li>
+        <li>user2</li>
+      </ul>
+    </div>
+  )
+}
